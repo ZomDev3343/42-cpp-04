e33db7c662be5394ca5ea82c6574a8933c14e957
zom <contact.zomzd@gmail.com>
2024-05-11 15:47:43 +0200

    goewhigowh

--- ex03/Character.cpp
+++ ex03/Character.cpp
@@ -1,18 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ICharacter.cpp                                     :+:      :+:    :+:   */
+/*   Character.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: truello <truello@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/10 18:32:09 by tohma             #+#    #+#             */
-/*   Updated: 2024/05/11 15:37:47 by truello          ###   ########.fr       */
+/*   Created: 2024/05/11 15:37:57 by truello           #+#    #+#             */
+/*   Updated: 2024/05/11 15:47:32 by truello          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ICharacter.hpp"
 
-ICharacter::~ICharacter(void)
+Character::Character(void)
 {
-	std::cout << "ICharacter destructor called" << std::endl;
+	std::cout << "Character constructor called" << std::endl;
+}
+
+Character::Character(const Character &ref)
+{
+	std::cout << "Character copy constructor called" << std::endl;
+}
+
+Character::~Character(void)
+{
+	std::cout << "Character destructor called" << std::endl;
+}
+
+Character &Character::operator=(const Character &ref)
+{
+	this->name = ref.getName();
+	//TODO
+	return (*this);
 }
